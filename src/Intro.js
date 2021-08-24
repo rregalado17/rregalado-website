96fd7e4c98117896b134ece353c36cf50fc80435
rregalado17 <rregalado17@gmail.com>
2021-08-24 00:31:01 -0400

    add components to app

---
+++ src/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Intro() {
+    return (
+      <div className="intro">
+        <header className="header">
+          <h3>Hi there, my name is Robinson Regalado and I am a fullstack software engineer. :)</h3>
+         <p>I am a graduate from the Flatiron School for Software Engineering. I am a Ruby of Rails developer and for frontend, I am a JavaScript developer working with ReactJS, GraphQL and others. I am currently looking to get hired fulltime or work on any interesting freelance projects.</p>
+        </header>
+      </div>
+    );
+  }
+
+export default Intro
---
+++ src/Projects.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Projects() {
+    return (
+        <div>
+            <h2>Here are some of the projects that I have worked on and continue updating in my spare time.</h2>
+            <h3><a href={"https://github.com/rregalado17/magisterial-messi-frontend"}>Frontend JS</a></h3>
+            <h3><a href={"https://github.com/rregalado17/magisterial-messi-backend"}>Backend Rails</a></h3>
+            <h3><a href={"https://github.com/rregalado17/litigation-manager-frontend"}>Litigation Manager Frontend React JS</a></h3>
+            <h3><a href={"https://github.com/rregalado17/litigation-manager-backend"}>Litigation Manager Rails</a></h3>
+            <h3><a href={"https://github.com/rregalado17/plutus-frontend"}>Plutus Frontend JS</a></h3>
+            <h3><a href={"https://github.com/rregalado17/plutus"}>Plutus Backend Rails</a></h3>
+
+        </div>
+    )
+}
+
+
+export default Projects 
