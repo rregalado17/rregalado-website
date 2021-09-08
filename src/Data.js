2f60dcd85efcb1b2dc0cd2c3ff49d61cdb3e9edb
rregalado17 <rregalado17@gmail.com>
2021-09-08 01:31:11 -0500

    refactor projects component

--- src/Data.js
+++ src/Data.js
@@ -1,17 +1,20 @@
 const PROJECTS =  [
   {   
+    image: "https://picsum.photos/200/300",
     name: "Magisterial Messi",
     frontend: "https://github.com/rregalado17/magisterial-messi-frontend",
     backend: "https://github.com/rregalado17/magisterial-messi-backend",
     youtube: "https://youtu.be/vow2ThkbVsA"
   },
   {
+    image: "https://picsum.photos/200/300",
     name: "Litigation Manager",
     frontend: "https://github.com/rregalado17/litigation-manager-frontend",
     backend: "https://github.com/rregalado17/litigation-manager-backend",
     youtube: "https://youtu.be/D4UI9hSygt8"
   },
   {
+    image: "https://picsum.photos/200/300",
     name: "Plutus",
     frontend: "https://github.com/rregalado17/plutus-frontend",
     backend: "https://github.com/rregalado17/plutus",
--- src/Projects.js
+++ src/Projects.js
@@ -4,24 +4,18 @@ function Projects({ myProjects }) {
     
     const projects = myProjects.map((project) => (
         <ul key={project.name}>
-          Project: {project.name}
-          <p>Frontend: {project.frontend}</p>
-          <p>Backend: {project.backend}</p>
-          <p>Video Walkthrough: {project.youtube}</p>
+         <img src={project.image} alt="Project Image"/><br/>
+          Project Name: {project.name}<br/>
+          <a href={project.frontend}>Github Frontend</a><br/>
+          <a href={project.backend}>Github Backend</a><br/>
+          <a href={project.youtube}>Video Walkthrough</a>
         </ul>
       ))
 
     return (
         <div>
-            <h2>Here are some of the projects that I have worked on and continue updating in my spare time.</h2>
+            <h3>Here are some of the projects that I have worked on and continue updating in my spare time.</h3>
             {projects}
-
-            {/* <h3><a href={"https://github.com/rregalado17/magisterial-messi-frontend"}>Frontend JS</a></h3>
-            <h3><a href={"https://github.com/rregalado17/magisterial-messi-backend"}>Backend Rails</a></h3>
-            <h3><a href={"https://github.com/rregalado17/litigation-manager-frontend"}>Litigation Manager Frontend React JS</a></h3>
-            <h3><a href={"https://github.com/rregalado17/litigation-manager-backend"}>Litigation Manager Rails</a></h3>
-            <h3><a href={"https://github.com/rregalado17/plutus-frontend"}>Plutus Frontend JS</a></h3>
-            <h3><a href={"https://github.com/rregalado17/plutus"}>Plutus Backend Rails</a></h3> */}
 
         </div>
     )
